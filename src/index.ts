e8faed627fb61d962060bd152d99a5a77ea35df3
Joao Tavares <joao.arthur.tavares@gmail.com>
2020-03-24 18:08:25 -0300

    fix(onExit & onSuccess): add a remove iframe to avoid mult postMessage calls

--- src/index.ts
+++ src/index.ts
@@ -1,96 +1,115 @@
-import { EventEmitter } from 'eventemitter3';
-import Render from './render';
+import { EventEmitter } from "eventemitter3";
+import Render from "./render";
 
 declare global {
-    interface Window { Connect: any; }
+ interface Window {
+  Connect: any;
+ }
 }
 
 class Connect {
-    hook: string;
-    payload: any;
-    domain: string;
-    session: string;
-    ee: any;
-    env: string;
-    getOrigin: any;
+ hook: string;
+ payload: any;
+ domain: string;
+ session: string;
+ ee: any;
+ env: string;
+ getOrigin: any;
 
-    constructor (props) {
-        this.domain = window.location.origin;
-        this.session = props.session;
-        this.ee = new EventEmitter();
-        this.env = props.env;
-        this.getOrigin = (env: string|void) => {
-            switch (env || this.env) {
-                case 'sandbox': return 'https://sandbox.quanto.app';
-                case 'production': return 'https://connect.quanto.app';
-                default : return this.env || this.getOrigin('production') ;
-            }
-        }
-        this.handlerMessageListener();
-        this.renderIframe();
-    };
+ constructor(props) {
+  this.domain = window.location.origin;
+  this.session = props.session;
+  this.ee = new EventEmitter();
+  this.env = props.env;
+  this.getOrigin = (env: string | void) => {
+   switch (env || this.env) {
+    case "sandbox":
+     return "https://sandbox.quanto.app";
+    case "production":
+     return "https://connect.quanto.app";
+    default:
+     return this.env || this.getOrigin("production");
+   }
+  };
+  this.handlerMessageListener();
+  this.renderIframe();
+ }
 
-    renderIframe  () {
-        Render(`${this.getOrigin()}/?hsession=${this.session}`, this.domain);
-    };
+ renderIframe() {
+  Render(`${this.getOrigin()}/?hsession=${this.session}`, this.domain);
+ }
 
-    handlerMessageListener () {
-        window.addEventListener('message', event => {
-            const { origin, data } = event;
-            const { hook, payload } = data;
-            if(origin !== this.getOrigin()) return;
-            this.payload = payload;
-            this.ee.emit(hook);
-        })
-    };
+ handleMessage(event) {
+  console.log("Evento, ", event);
+  const { origin, data } = event;
+  const { hook, payload } = data;
+  console.log(origin, this.env);
+  if (origin !== this.getOrigin()) return;
+  this.payload = payload;
+  this.ee.emit(hook);
+ }
 
-    onLoad (callback) {
-        this.ee.on('onload', () => {
-            callback();
-        });
-    };
+ handlerMessageListener() {
+  window.addEventListener("message", (event) => {
+   const { origin, data } = event;
+   const { hook, payload } = data;
+   if (origin !== this.getOrigin()) return;
+   this.payload = payload;
+   this.ee.emit(hook);
+  });
+ }
 
-    onSuccess (callback) {
-        this.ee.on('onsuccess', () => {
-            callback();
-        });
-    };
+ onLoad(callback) {
+  this.ee.on("onload", () => {
+   callback();
+  });
+ }
 
-    onExit (callback) {
-        this.ee.on('onexit', () => {
-            callback();
-        });
-    };
+ onSuccess(callback) {
+  this.ee.on("onsuccess", () => {
+   callback();
+   const ct = document.querySelector("#connect-embedded");
+   if (ct !== null) ct.remove();
+  });
+ }
 
-    onGrantedPermission (callback) {
-        this.ee.on('ongrantedpermission', () => {
-            callback(this.payload);
-        });
-    };
+ onExit(callback) {
+  this.ee.on("onexit", () => {
+   callback();
+   const ct = document.querySelector("#connect-embedded");
+   if (ct !== null) ct.remove();
+  });
+ }
 
-    onAuthSuccess (callback) {
-        this.ee.on('onauthsuccess', () => {
-            callback(this.payload);
-        });
-    };
+ onGrantedPermission(callback) {
+  this.ee.on("ongrantedpermission", () => {
+   callback(this.payload);
+  });
+ }
 
-    onAuthFail (callback) {
-        this.ee.on('onauthfail', () => {
-            callback(this.payload);
-        });
-    };
+ onAuthSuccess(callback) {
+  this.ee.on("onauthsuccess", () => {
+   callback(this.payload);
+  });
+ }
 
-    onSelectBank (callback) {
-        this.ee.on('onselectbank', () => {
-            callback(this.payload);
-        });
-    };
+ onAuthFail(callback) {
+  this.ee.on("onauthfail", () => {
+   callback(this.payload);
+  });
+ }
 
-    onAddAccountSuccess (callback) {
-        this.ee.on('onaddaccountsuccess', () => {
-            callback(this.payload);
-        });
-    };
+ onSelectBank(callback) {
+  this.ee.on("onselectbank", () => {
+   callback(this.payload);
+  });
+ }
+
+ onAddAccountSuccess(callback) {
+  this.ee.on("onaddaccountsuccess", () => {
+   callback(this.payload);
+  });
+ }
 }
 
 window.Connect = window.Connect || Connect;
